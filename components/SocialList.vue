d364a81b6981dadd90e55b7cef01b1e2a5fee7bd
Elia Guarnieri <elia.guarnieri@gmail.com>
2021-05-20 15:25:05 +0200

    Creata bozza SocialList

---
+++ components/SocialList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul class="social__list">
+    <li class="social__item">
+      L
+    </li>
+    <li class="social__item">
+      I
+    </li>
+    <li class="social__item">
+      F
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.social {
+  &__list {
+    display: flex;
+    padding: 1em 0;
+  }
+
+  &__item + &__item {
+    margin-left: 1em;
+  }
+}
+</style>
