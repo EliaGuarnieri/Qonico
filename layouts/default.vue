2452f97c6c348ad4538fc59c6793a6819e0e90a1
Elia Guarnieri <elia.guarnieri@gmail.com>
2021-05-20 15:26:03 +0200

    Inserita navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <TheNavbar />
     <Nuxt />
   </div>
 </template>
+
+<script>
+import TheNavbar from '~/components/TheNavbar'
+
+export default {
+  components: { TheNavbar }
+}
+</script>
