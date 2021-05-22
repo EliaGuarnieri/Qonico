8f0f8b3115a0e46d14355d97e99df905f37c1200
Elia Guarnieri <elia.guarnieri@gmail.com>
2021-05-22 13:33:29 +0200

    Inserita sezione servizi

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <div />
+  <main>
+    <CardSection />
+  </main>
 </template>
 
 <script>
+import CardSection from 'components/CardsSection'
+
 export default {
+  components: { CardSection },
   head () {
     return {
       script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
