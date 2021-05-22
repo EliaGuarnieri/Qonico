8a9974284bf280fb02759a9fa27eac553f7e0369
Elia Guarnieri <elia.guarnieri@gmail.com>
2021-05-22 13:34:15 +0200

    Creata card section

---
+++ components/CardsSection/TheCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="card">
+    <h3 class="title card__title">
+      {{ title }}
+    </h3>
+    <div class="card__body">
+      <p>{{ body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: null
+    },
+    body: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  padding: 5rem;
+  box-shadow: 0px 20px 34px 13px #e8edfc;
+  border-radius: 20px;
+
+  &__title {
+    color: $blue;
+  }
+
+  @include for-tablet {
+    width: calc(50% - calc(#{$gutter} / 2));
+  }
+
+  @include for-desktop {
+    width: calc(33% - calc(#{$gutter} / 2));
+  }
+}
+
+.card + .card {
+  margin-top: $gutter;
+
+  &:nth-child(2) {
+    @include for-tablet {
+      margin-top: 0;
+    }
+  }
+
+  &:nth-child(3) {
+    @include for-desktop {
+      margin-top: 0;
+    }
+  }
+}
+</style>
---
+++ components/CardsSection/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="section">
+    <div class="cards container row">
+      <TheCard
+        v-for="(servizio, index) in servizi"
+        :key="index"
+        :title="servizio.title"
+        :body="servizio.body"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+import TheCard from './TheCard'
+
+export default {
+  components: { TheCard },
+  data: () => ({
+    servizi: [
+      {
+        title: 'Titolo',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim.'
+      },
+      {
+        title: 'Titolo',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim.'
+      },
+      {
+        title: 'Titolo',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim.'
+      },
+      {
+        title: 'Titolo',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim.'
+      },
+      {
+        title: 'Titolo',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim.'
+      },
+      {
+        title: 'Titolo',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim.'
+      }
+    ]
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+.cards {
+  display: flex;
+  flex-flow: wrap;
+}
+</style>
