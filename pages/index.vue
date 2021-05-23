eafc4cc15472ef8a056448845a1a0d1cce6b76cb
Elia Guarnieri <elia.guarnieri@gmail.com>
2021-05-23 11:53:47 +0200

    Inserita sezione chi siamo

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
   <main>
     <SectionServizi />
+    <SectionChiSiamo />
   </main>
 </template>
 
 <script>
 import SectionServizi from 'components/sections/SectionServizi'
+import SectionChiSiamo from 'components/sections/SectionChiSiamo'
 
 export default {
-  components: { SectionServizi },
+  components: { SectionServizi, SectionChiSiamo },
   head () {
     return {
       script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
