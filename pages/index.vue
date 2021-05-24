0d34ba4b316babf2bdccaf7f3e3b2ec82330a945
Elia Guarnieri <elia.guarnieri@gmail.com>
2021-05-24 09:09:37 +0200

    Importata sezione contatti

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,17 @@
   <main>
     <SectionServizi />
     <SectionChiSiamo />
+    <SectionContatti />
   </main>
 </template>
 
 <script>
 import SectionServizi from 'components/sections/SectionServizi'
 import SectionChiSiamo from 'components/sections/SectionChiSiamo'
+import SectionContatti from 'components/sections/SectionContatti'
 
 export default {
-  components: { SectionServizi, SectionChiSiamo },
+  components: { SectionServizi, SectionChiSiamo, SectionContatti },
   head () {
     return {
       script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
