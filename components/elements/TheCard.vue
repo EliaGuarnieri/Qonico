68b3b326bf220664abf688211cca8c66557398b0
Elia Guarnieri <elia.guarnieri@gmail.com>
2021-06-08 15:07:14 +0200

    Modificata sezione servizi

--- components/elements/TheCard.vue
+++ components/elements/TheCard.vue
@@ -1,17 +1,34 @@
 <template>
   <div class="card">
-    <h3 class="title card__title">
-      {{ title }}
-    </h3>
-    <div class="card__body">
-      <p>{{ body }}</p>
+    <div
+      class="card__media"
+      :class="{'is-right': isOdd}"
+    >
+      <img :src="require(`images/${image}`)">
     </div>
+    <div class="card__content">
+      <h3 class="title card__title">
+        {{ title }}
+      </h3>
+      <div class="card__body">
+        <p>{{ body }}</p>
+      </div>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
 export default {
   props: {
+    index: {
+      type: Number,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    },
     title: {
       type: String,
       default: null
@@ -20,18 +37,47 @@ export default {
       type: String,
       default: null
     }
+  },
+  computed: {
+    isOdd () {
+      return this.index % 2 !== 0
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .card {
-  padding: 5rem;
-  box-shadow: 0px 20px 34px 13px #e8edfc;
-  border-radius: 20px;
+  display: flex;
+  flex-flow: column;
+
+  &__media {
+    display: inline-block;
+    margin: 0 auto $gutter;
+    min-width: 20.5rem;
+  }
 
   &__title {
     color: $blue;
   }
+}
+
+@include for-tablet {
+  .card {
+    flex-flow: row;
+    align-items: center;
+
+    &__media {
+      margin: 0 $gutter 0 0;
+    }
+  }
+
+  .is-right {
+    order: 1;
+    margin: 0 0 0 $gutter;
+  }
+}
+.card + .card {
+  margin-top: $gutter-large;
 }
 </style>
--- components/sections/SectionServizi.vue
+++ components/sections/SectionServizi.vue
@@ -4,9 +4,10 @@
       <TheCard
         v-for="(servizio, index) in servizi"
         :key="index"
+        :index="index"
+        :image="servizio.image"
         :title="servizio.title"
         :body="servizio.body"
-        class="card"
       />
     </div>
   </section>
@@ -20,28 +21,34 @@ export default {
   data: () => ({
     servizi: [
       {
-        title: 'Titolo',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim.'
+        image: 'creazione-siti.svg',
+        title: 'Creazione siti web, E-Commerce e Landing Page',
+        body: 'Creiamo siti web ed E-Commerce responsive e intuitivi. Realizziamo blog e landing page dinamiche e funzionali. Eseguiamo anche restyling, manutenzione e assistenza tecnica su siti web esistenti. Contattaci e parlaci della tua attività, ti proporremo le migliori soluzioni per realizzare il tuo sito web.'
       },
       {
-        title: 'Titolo',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim.'
+        image: 'branding.svg',
+        title: 'Branding e Grafica',
+        body: 'Un\'immagine curata nei dettagli è il primo biglietto da visita per fare una buona impressione e conquistare i clienti. Realizziamo loghi, biglietti da visita, brochure, volantini, grafiche, illustrazioni e molto altro. Contattaci, ti aiuteremo a creare o a rinnovare l\'immagine, online o cartacea, del tuo brand o della tua attività.'
       },
       {
-        title: 'Titolo',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim.'
+        image: 'social-strategy.svg',
+        title: 'Social Media Strategy',
+        body: 'Essere presenti sui Social oggi non è solo una necessità, ma anche una grande opportunità. Curiamo la tua presenza sui Social Media in modo da ottenere il maggior coinvolgimento possibile dei tuoi follower, raggiungere nuovi clienti e generare conversioni. Gestiamo il tuo business sui Social attraverso attività di Management e Marketing. Ti aiuteremo a individuare i canali adatti alla tua attività e imposteremo la tua comunicazione in modo efficace. Contattaci per sapere qual è la soluzione più adatta a te.'
       },
       {
-        title: 'Titolo',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim.'
+        image: 'webmarketing.svg',
+        title: 'Web Marketing e Comunicazione',
+        body: 'Realizziamo campagne pubblicitarie mirate ed efficaci per diffondere i tuoi valori e far conoscere il tuo brand a nuovi potenziali clienti. Nell\'epoca iper-digitale nella quale viviamo, una buona comunicazione online è essenziale per distinguerti dalla concorrenza ed emergere. Contattaci e raccontaci quello che fai, ti aiuteremo a comunicarlo sul web.'
       },
       {
-        title: 'Titolo',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim.'
+        image: 'creazione-contenuti.svg',
+        title: 'Creazione di contenuti e Piani Editoriali',
+        body: 'Un piano editoriale strategico è ciò che consente di comunicare con il proprio pubblico in modo accurato e coerente. Organizziamo la tua comunicazione online e creiamo contenuti accattivanti per i tuoi Social, il tuo blog o il tuo sito web. Foto professionali, contenuti grafici, illustrazioni, video, gif, articoli e post Social ti permetteranno di comunicare in modo strategico su tutti i tuoi canali. Contattaci e scopri il pacchetto più adatto alle tue esigenze.'
       },
       {
-        title: 'Titolo',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim.'
+        image: 'seo.svg',
+        title: 'Ottimizzazione SEO e SEM',
+        body: 'Che si tratti di di un prodotto o di un servizio, un buon posizionamento sui motori di ricerca è essenziale per avere maggiore visibilità. Attraverso l\'ottimizzazione SEO, On Site e Off Site, del tuo sito web e l\'analisi delle Keyword, potrai raggiungere tutti quei clienti che ancora non ti conoscono. Contattaci per ampliare la tua presenza online.'
       }
     ]
   })
@@ -52,31 +59,5 @@ export default {
 .cards {
   display: flex;
   flex-flow: wrap;
-}
-
-.card {
-  @include for-tablet {
-    width: calc(50% - calc(#{$gutter} / 2));
-  }
-
-  @include for-desktop {
-    width: calc(33% - calc(#{$gutter} / 2));
-  }
-}
-
-.card + .card {
-  margin-top: $gutter;
-
-  &:nth-child(2) {
-    @include for-tablet {
-      margin-top: 0;
-    }
-  }
-
-  &:nth-child(3) {
-    @include for-desktop {
-      margin-top: 0;
-    }
-  }
 }
 </style>
