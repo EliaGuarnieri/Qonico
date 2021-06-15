58f25aaedb1a96e8d740563da9d4a5bd3f331b3e
Elia Guarnieri <elia.guarnieri@gmail.com>
2021-06-15 17:58:37 +0200

    Rimosso particle.js

--- components/TheHeader/TheHero.vue
+++ components/TheHeader/TheHero.vue
@@ -11,33 +11,14 @@
   </div>
 </template>
 
-<script>
-import { tsParticles } from 'tsparticles'
-import particlesSettings from 'assets/particles/particles.json'
-/* eslint-disable */
-export default {
-  data: () =>({
-    particlesSettings: particlesSettings
-  }),
-  mounted () {
-    this.$nextTick(() => {
-      this.initParticleJS()
-    })
-  },
-  methods: {
-    initParticleJS () {
-      tsParticles.load('hero', this.particlesSettings)
-    }
-  }
-}
-</script>
-
 <style lang="scss" scoped>
 .hero {
   display: flex;
   align-items: center;
   justify-content: center;
   height: calc(100vh - 60px);
+  background: url("images/cool-background.svg");
+  background-size: cover;
 }
 .container {
   position: absolute;
