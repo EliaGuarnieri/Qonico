d158a0a4e5e84c2514d20b9ccb1417054fc0f6d5
Elia Guarnieri <elia.guarnieri@gmail.com>
2021-08-25 11:13:50 +0200

    Aggiunto Alert

---
+++ components/Alert.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="alert"
+    :class="{ error: state === 'error', success: state === 'success' }"
+  >
+    <small>{{ message }}</small>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true
+    },
+    state: {
+      type: String,
+      required: true,
+      validator: value => ['success', 'error'].includes(value)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.alert {
+  display: flex;
+  justify-self: center;
+  width: fit-content;
+  white-space: break-spaces;
+  color: #fff;
+  padding: 1.2rem 1.8rem;
+  border-radius: 3px;
+}
+.error {
+  background-color: red;
+}
+.success {
+  background-color: green;
+}
+</style>
--- components/TheForm.vue
+++ components/TheForm.vue
@@ -29,13 +29,76 @@
       <CheckBox v-model="form.privacy" />
 
       <div class="form__footer">
+        <transition name="slide-fade-right">
+          <span
+            v-if="submit.loading"
+            class="loader"
+          >
+            <svg
+              viewBox="0 0 100 100"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                cx="50"
+                cy="50"
+                r="45"
+              />
+            </svg>
+          </span>
+        </transition>
         <button
           class="button"
           type="submit"
+          :disabled="submit.loading"
+          :class="{
+            success: submit.success,
+            error: submit.error
+          }"
         >
-          Invia
+          <span class="text">Invia</span>
+          <transition name="slide-fade-right">
+            <span
+              v-if="!submit.loading"
+              class="arrow"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 350 100"
+              >
+                <defs>
+                  <marker
+                    id="arrowhead"
+                    markerWidth="10"
+                    markerHeight="7"
+                    refX="0"
+                    refY="3.5"
+                    orient="auto"
+                    fill="#fff"
+                  >
+                    <polygon points="0 0, 10 3.5, 0 7" />
+                  </marker>
+                </defs>
+                <line
+                  x1="0"
+                  y1="50"
+                  x2="250"
+                  y2="50"
+                  stroke="#fff"
+                  stroke-width="10"
+                  marker-end="url(#arrowhead)"
+                />
+              </svg>
+            </span>
+          </transition>
         </button>
       </div>
+      <transition name="fade-error">
+        <Alert
+          v-if="submit.loaded"
+          :message="submit.message"
+          :state="submit.state"
+        />
+      </transition>
     </form>
   </formwrapper>
 </template>
@@ -47,6 +110,7 @@ import InputName from '~/components/elements/InputName.vue'
 import InputEmail from '~/components/elements/InputEmail.vue'
 import InputMessaggio from '~/components/elements/InputMessaggio.vue'
 import CheckBox from '~/components/elements/CheckBox.vue'
+import Alert from '~/components/Alert.vue'
 
 export default {
   components: {
@@ -54,7 +118,8 @@ export default {
     InputName,
     InputEmail,
     InputMessaggio,
-    CheckBox
+    CheckBox,
+    Alert
   },
   props: {
     name: {
@@ -68,6 +133,12 @@ export default {
       email: '',
       messaggio: '',
       privacy: false
+    },
+    submit: {
+      message: '',
+      loading: false,
+      loaded: false,
+      state: ''
     }
   }),
   validations: {
@@ -87,8 +158,17 @@ export default {
         .join('&')
     },
     handleSubmit () {
+      this.submit.loaded = false
+      this.submit.loading = true
+
       this.$v.form.$touch()
-      if (this.$v.form.$pending || this.$v.form.$error) { return }
+      if (this.$v.form.$pending || this.$v.form.$error) {
+        this.submit.loading = false
+        this.submit.loaded = true
+        this.submit.state = 'error'
+        this.submit.message = 'Oh no! 😱 Sembra che tu non abbia compilato tutti i campi!\r\nProva a correggere gli errori e ritenta.'
+        return
+      }
 
       const formData = {
         Nome: this.form.nome,
@@ -104,11 +184,22 @@ export default {
           'form-name': this.name,
           ...formData
         })
+      }).then(() => {
+        this.submit.loading = false
+        this.submit.loaded = true
+        this.submit.state = 'success'
+        this.submit.message = 'Grazie per averci contattato!\r\nPresto avrai nostre notizie 😎'
         // eslint-disable-next-line no-console
-      }).then(() => console.log('form submitted'))
-
-      // eslint-disable-next-line no-console
-        .catch(error => console.error(error))
+        console.log('form submitted')
+      })
+        .catch((error) => {
+          this.submit.loading = false
+          this.submit.loaded = true
+          this.submit.state = 'error'
+          this.submit.message = 'Oh no! 😱 Qualcosa è andato storto.\r\nTi va di riprovare?'
+          // eslint-disable-next-line no-console
+          console.error(error)
+        })
     }
   }
 }
@@ -123,15 +214,91 @@ export default {
   &__footer {
     display: flex;
     justify-content: flex-end;
+    align-items: center;
   }
 }
 .button {
+  position: relative;
+  display: flex;
+  align-items: center;
   background-color: $blue;
   color: $white;
   border-radius: 3px;
+  max-width: 118px;
+  transition: max-width 0.25s cubic-bezier(0.25, 0.46, 0.45, 0.94);
 
   &:hover {
     background-color: $blue-dark;
+  }
+
+  &[disabled="disabled"] {
+    max-width: 83px;
+
+    &:hover {
+      background-color: $blue;
+      cursor: wait;
+    }
+  }
+
+  .arrow {
+    position: relative;
+    margin-left: 1rem;
+    width: 25px;
+    height: 17px;
+    line-height: 0.7;
+  }
+}
+
+.loader {
+  position: relative;
+  margin-right: 1.8rem;
+  width: 20px;
+  height: 20px;
+
+  svg {
+    inset: 0;
+    position: absolute;
+    animation: 2s linear infinite svg-animation;
+    max-width: 100px;
+  }
+
+  circle {
+    animation: 1.4s ease-in-out infinite both circle-animation;
+    display: block;
+    fill: transparent;
+    stroke: $black;
+    stroke-linecap: round;
+    stroke-dasharray: 283;
+    stroke-dashoffset: 280;
+    stroke-width: 10px;
+    transform-origin: 50% 50%;
+  }
+}
+
+@keyframes circle-animation {
+  0%,
+  25% {
+    stroke-dashoffset: 280;
+    transform: rotate(0);
+  }
+
+  50%,
+  75% {
+    stroke-dashoffset: 75;
+    transform: rotate(45deg);
+  }
+
+  100% {
+    stroke-dashoffset: 280;
+    transform: rotate(360deg);
+  }
+}
+@keyframes svg-animation {
+  0% {
+    transform: rotateZ(0deg);
+  }
+  100% {
+    transform: rotateZ(360deg);
   }
 }
 </style>
