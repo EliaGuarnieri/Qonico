517c5ccd5329ab7eda6c5a94ee6e622f6b500874
Elia Guarnieri <elia.guarnieri@gmail.com>
2021-09-08 17:46:31 +0200

    change path

--- functions/getInstagramFeed.js
+++ functions/getInstagramFeed.js
@@ -4,9 +4,9 @@ const axios = require('axios')
 const connect = require('connect')
 const app = connect()
 
-const feed = []
+const path = process.env.NODE_ENV === 'production' ? '/.netlify/functions' : '/functions'
 
-app.use('/functions', async (req, res) => {
+app.use(path, async (req, res) => {
   const baseURL = process.env.NODE_ENV === 'production' ? process.env.URL : 'http://192.168.1.6:3000'
   const currentUrl = new URL(req.url, baseURL)
   const queries = currentUrl.searchParams
@@ -15,6 +15,7 @@ app.use('/functions', async (req, res) => {
     params: { access_token: process.env.INSTAGRAM_ACCESS_TOKEN, fields: queries.get('fields') }
   })
     .then((response) => {
+      const feed = []
       if (response.status === 400) {
         res.statusCode = response.status
         return response.status
