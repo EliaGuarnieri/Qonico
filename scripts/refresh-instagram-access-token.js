8be801041e1f93ca111a1d7fc3f16b9d66d03d9b
Elia Guarnieri <elia.guarnieri@gmail.com>
2021-09-10 11:17:28 +0200

    Working refresh INSTAGRAM_ACCESS_TOKEN

--- scripts/refresh-instagram-access-token.js
+++ scripts/refresh-instagram-access-token.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-console */
-const fs = require('fs')
 const axios = require('axios')
-
-console.log(process.env.INSTAGRAM_ACCESS_TOKEN)
+const NetlifyAPI = require('netlify')
 
 const refreshAccessToken = async () => {
   const freshAccessToken = await axios.get('https://graph.instagram.com/refresh_access_token', {
@@ -22,6 +20,23 @@ const refreshAccessToken = async () => {
 }
 
 refreshAccessToken().then((response) => {
-  console.log(response.expires_in)
-  fs.writeFileSync('./.env', `INSTAGRAM_ACCESS_TOKEN=${response.access_token}\n`)
+  const updateNetlifyEnv = async function () {
+    const client = new NetlifyAPI('zJwjwqy1CXNSdJWq64h_2wo7aPVnqcgN0NupmGlu67E')
+    try {
+      const sites = await client.updateSite({
+        site_id: 'a3dd5d30-3267-407b-bd31-fa70b249ff9d',
+        body: {
+          build_settings: {
+            env: {
+              INSTAGRAM_ACCESS_TOKEN: response.access_token
+            }
+          }
+        }
+      })
+      return sites
+    } catch (error) {
+      return error
+    }
+  }
+  return updateNetlifyEnv
 })
